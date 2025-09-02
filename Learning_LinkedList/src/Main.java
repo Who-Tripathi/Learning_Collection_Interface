8590022e4fbcac17d27cd0a1ad73a51114d97d80
who-tripathi <whotripathi@gmail.com>
2025-09-02 21:54:26 +0530

    Everything of the project is added with the second exercise of the LinkedList

--- Learning_LinkedList/src/Main.java
+++ Learning_LinkedList/src/Main.java
@@ -1,15 +1,77 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.logging.StreamHandler;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+//        1. Browser History Simulation
+//
+//        Use a LinkedList<String> to simulate browsing history.
+//
+//        LinkedList<String> browserHistory = new LinkedList<>();
+//
+//        System.out.printf("Welcome to the browser history manager.\n 1. Type : 'Search' to add history to the browser.\n 2. Type : 'History' to check your browsing history.\n 3. Type : 'Delete' to clear your browsing history.\n 4. Type : 'Back' to go back to the last visited page\n 5. Type : 'Last' to get the last visited page of the browser.\n 6. Type : 'End' to end the Browser.\n");
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        Scanner history=new Scanner(System.in);
+//
+//
+//        String selected;
+//        do {
+//            selected = sc.next();
+//            switch (selected) {
+//                case "Search": {
+//                    System.out.print("Enter your search Topic : ");
+//                    browserHistory.addLast(history.nextLine());
+//                }break;
+//                case "History": {
+//                    System.out.println(browserHistory);
+//                }break;
+//                case "Delete": {
+//                    browserHistory.clear();
+//                    System.out.println("Browsing History is cleared...!");
+//                }break;
+//                case "Back": {
+//                    System.out.println("Your previous browsing history is : "+browserHistory.pollLast());
+//                }break;
+//                case "Last": {
+//                    System.out.println("Your previous browsing history is : "+browserHistory.getLast());
+//                }break;
+//                default: {
+//                    System.out.println("Invalid Selection, please select right choice");
+//                }
+//            }
+//        } while (!(selected.equalsIgnoreCase("end")));
+
+//        2. Bank Queue Simulation
+//
+//        Use a LinkedList<String> as a queue of customers.
+
+        LinkedList<String> customers=new LinkedList<>();
+        System.out.print("Add the customers in the Queue : " );
+        Scanner scanner=new Scanner(System.in);
+        int i=0;
+        while(i<5){
+            customers.offer(scanner.nextLine());
+            i++;
         }
+
+        System.out.print("5 Customers are added in the the queue Type Y/N to proceed checkout: ");
+        String s=scanner.nextLine();
+        if(s.equalsIgnoreCase("Y")){
+            String customer;
+            while ((customer=customers.pollFirst())!=null){
+                System.out.println("Bank is serving : "+customer);
+                try{Thread.sleep(3000);}catch (InterruptedException e){};
+            }
+        }else{
+            System.out.println("Bank System is closed...Thanks for using the App...!");
+        }
+        System.out.println("Thanks for using the App...!");
     }
 }
